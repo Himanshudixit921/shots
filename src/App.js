95702093b10a49a070ef0437714d14cc58f52e55
Himanshudixit921 <himanshudixit921@gmail.com>
2024-06-08 01:37:46 +0530

    title changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
-import store from "./app/store";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
+import store from "./app/store";
 import CanvasContainer from "./components/Workspace";
 
 function App() {
+  useEffect(() => {
+    document.title = "Shots";
+  }, []);
+
   return (
     <Provider store={store}>
       <div>
